b3de8d22cc0d236783bfd3fbde4dd9d440639c38
christophergalapin <christopher.galapin@telusinternational.com>
2023-09-26 20:37:06 +0800

    EAFT-141

--- src/components/Subheader.tsx
+++ src/components/Subheader.tsx
@@ -1,11 +1,40 @@
 import React, { useEffect, useState  }  from 'react'
 import { StyledStarDiv,StyledEventDiv,StyledTaskDiv,StyledCOPDiv, TitleDiv, IconDiv, ValueDiv } from '../components/Div/Div.styles'
-
-
+import { useAppDispatch, useAppSelector } from '../redux/store'
+import { TeamMemberInfoState, fetchTeamMemberInfo } from '../redux/teamMemberInfoSlice'
 
 export const Subheader = () => {
 
-	
+	const name = localStorage.getItem('givenName') + ' ' + localStorage.getItem('familyName')
+
+	type TeamMember = {
+		workdayId: number
+		employeeName: string
+		practice: string
+		functionalArea: string
+		totalStars: number
+		starPoints: number
+		starPesoConversion: string
+		stars2022: number
+		stars2023: number
+		starPointsDeducted: number
+		copPoints: number
+		copPesoConversion: number
+		cop2022Points: number
+		cop2023Points: number
+		copPointsDeducted: number
+	  }
+
+	const teamMember = useAppSelector((state: any) => state.teamMemberInfoSlice)
+  	const dispatch = useAppDispatch()
+	const [teamMemberInfo, setTeamMemberInfo] = useState<TeamMember[]>([])
+
+	useEffect(() => {
+		dispatch(fetchTeamMemberInfo(name)).then((data: any) => {
+		  const teamMemberInfoArray = Object.values(data.payload)
+		  setTeamMemberInfo(teamMemberInfoArray as TeamMember[])
+		})
+	  }, [])
 	
 	return (
 		
@@ -13,7 +42,7 @@ export const Subheader = () => {
 		    <StyledStarDiv>
 				<TitleDiv>Star Points</TitleDiv>
 				<IconDiv><img src={require('../assets/images/Star.png')} /></IconDiv>
-				<ValueDiv>9,999</ValueDiv>
+				<ValueDiv>{teamMemberInfo.at(0)?.starPoints || 0}</ValueDiv>
 			</StyledStarDiv>
 
 			<StyledEventDiv>
@@ -31,7 +60,7 @@ export const Subheader = () => {
 			<StyledCOPDiv>
 				<TitleDiv>COP</TitleDiv>
 				<IconDiv><img src={require('../assets/images/COP.png')} /></IconDiv>
-				<ValueDiv>10</ValueDiv>
+				<ValueDiv>{teamMemberInfo.at(0)?.copPoints || 0}</ValueDiv>
 			</StyledCOPDiv>
 		</div>
 		
---
+++ src/redux/teamMemberInfoSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction, createAsyncThunk  } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { RootState } from './store'
+
+export interface TeamMemberInfoState {
+    loading: boolean,
+	teamMemberInfo: [],
+	error: string
+}
+
+const initialState: TeamMemberInfoState = {
+	loading: false,
+	teamMemberInfo: [],
+	error: ''
+}
+
+export const fetchTeamMemberInfo = createAsyncThunk('getTeamMemberInfoByName', async (name: string) => {
+	return axios
+	  .get(`http://localhost:3001/teamMember/getTeamMemberInfoByName/${name}`)
+	  .then(response => response.data)
+})
+
+export const teamMemberInfoSlice = createSlice({
+	name: 'teamMemberInfo',
+	initialState,
+	reducers: {
+		storeTeamMemberInfo: (state, action: PayloadAction<[]>) => {
+			state.teamMemberInfo = action.payload
+		}
+	},
+	extraReducers: builder => {
+		builder.addCase(fetchTeamMemberInfo.pending, state => {
+			state.loading = true
+		})
+		builder.addCase(fetchTeamMemberInfo.fulfilled, (state, action) => {
+			state.loading = false
+			state.teamMemberInfo = action.payload
+			state.error = ''
+		})
+		builder.addCase(fetchTeamMemberInfo.rejected, (state, action) => {
+			state.loading = false
+			state.teamMemberInfo = []
+			state.error = action.error.message!
+		})
+	}
+})
+
+export default teamMemberInfoSlice.reducer
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const getTeamMemberInfoByName = (state: RootState) => state.teamMemberInfo
